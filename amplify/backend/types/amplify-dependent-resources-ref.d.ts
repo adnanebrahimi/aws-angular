6bcb3b8480ff961e25552c3362400e6e82fe5f5f
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2021-11-29 18:02:18 +0330

    Add API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "awsangular": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
